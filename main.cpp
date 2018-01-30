80a0c0fec20a642cadac2b4bdefaf81bafef7e22
lopezfjose <jflopezfernandez@gmail.com>
2018-01-30 11:52:17 -0500

    Still setting up project

---
+++ main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by jflop on 1/30/2018.
+//
+
+#include "main.h"
+
+int main() {
+    std::cout << "testing...\n";
+}
---
+++ main.h
@@ -0,0 +1,15 @@
+//
+// Created by jflop on 1/30/2018.
+//
+
+#ifndef PROJECT_MAIN_H
+#define PROJECT_MAIN_H
+
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <string_view>
+#include <vector>
+
+#endif //PROJECT_MAIN_H
