914a2e9cc63ccbb4101483e4f44601d57df5af2a
Jose Fernando Lopez Fernandez <jflopezfernandez@gmail.com>
2018-01-31 00:46:47 -0500

    Added the files that correspond to the CMakeLists.txt file I'm using for testing to get a baseline from which I can get appveyor running. [ci skip] --skip-ci

---
+++ test/test_producer.cpp
@@ -0,0 +1,16 @@
+
+#include <catch/catch.hpp>
+#include <producer.h>
+
+#include <iostream>
+
+TEST_CASE("producer: constructor / destructor")
+{
+	CHECK_NOTHROW(producer{});
+}
+
+TEST_CASE("producer: print msg")
+{
+	producer p{};
+	CHECK_NOTHROW(p.print_msg());
+}
